e0ef35adddd8e3d5e190b0edc914af4463d4df21
Hasankhan170 <hasankhanali0316@gmail.com>
2025-01-06 18:14:32 +0500

    almost done

--- src/controllers/order.controllers.js
+++ src/controllers/order.controllers.js
@@ -8,6 +8,7 @@ import Order from "../models/user.order.js"
 const orderCreate = async (req,res)=>{
     const {id,quantity} = req.body
     const userId = req.user.id;
+
     const user = await Users.findById(userId)
     if(!user) return res.status(404).json({message:"User not found"})
      if (user.role !== "customer") {
@@ -19,7 +20,7 @@ const orderCreate = async (req,res)=>{
     const totalPrice = product.price * quantity
     const order = await Order.create({
         user: userId,
-        product: [id],
+        products: [id],
         quantity,
         totalPrice,
         status: "Order Done"
@@ -29,6 +30,7 @@ const orderCreate = async (req,res)=>{
     await user.updateOne({
         $push: {orders: order._id}
     })
+
 
     const productOwner = await Users.findById(product.user); 
     if (productOwner) {
@@ -46,4 +48,27 @@ const orderCreate = async (req,res)=>{
 
 }
 
-export {orderCreate}
+const orderList = async (req,res)=>{
+    const userId = req.user.id;
+    const user = await Users.findById(userId).populate("orders")
+    if(!user) return res.status(404).json({message:"User not found"})
+    
+    res.status(200).json({
+        message:"Order list",
+        orders: user.orders
+    })
+}
+
+const singleOrder = async (req,res)=>{
+    const {id} = req.params;
+    const user = await Order.findById(id).populate("products")
+    if(!user) return res.status(404).json({message:"User not found"})
+
+    res.status(200).json({
+        message:"Single order",
+        order: user
+    })
+    
+}
+
+export {orderCreate,orderList,singleOrder}
--- src/routes/order.route.js
+++ src/routes/order.route.js
@@ -1,8 +1,12 @@
 import express from 'express';
-import { orderCreate } from '../controllers/order.controllers.js';
+import { orderCreate,orderList,singleOrder } from '../controllers/order.controllers.js';
 import authenticateUser from "../middleware/auth.middleware.js"
 const router = express.Router()
 
 router.post("/orders",authenticateUser,orderCreate)
+router.get("/orders",authenticateUser,orderList)
+router.get("/orders/:id",authenticateUser,singleOrder)
+
+
 
 export default router;
