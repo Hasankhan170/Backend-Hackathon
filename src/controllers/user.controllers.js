5141f27a73e5003dc561ac0eaa6cfe6f779f8c7a
Hasankhan170 <hasankhanali0316@gmail.com>
2025-01-06 18:27:55 +0500

    user controllers done

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -3,19 +3,20 @@ import bcrypt from "bcrypt";
 import User from "../models/user.models.js";
 import { sendWelcomeEmail } from "../utils/nodemailer.utils.js";
 
-
+//generate access token
 const generateAccessToken = (user) => {
     return jwt.sign({ email: user.email, id: user._id}, process.env.ACCESS_TOKEN, { expiresIn: '6h' });
 };
 
 
-
+//generate refresh token
 const generateRefreshToken = (user)=>{
     return jwt.sign({email:user.email},process.env.REFRESH_TOKEN,{
         expiresIn: '7d'
     })
 }
 
+// register user
 const register = async(req,res)=>{
     const {username,email,password,role} = req.body
     if(!username) return res.status(404).json({message:"Please enter a name"})
@@ -26,6 +27,7 @@ const register = async(req,res)=>{
         return res.status(400).json({ message: "Invalid role" });
     }
 
+   try {
     const user = await User.findOne({email: email})
     if(user) return res.status(400).json({message:"User already exists"})
 
@@ -37,9 +39,6 @@ const register = async(req,res)=>{
             ...(role === 'admin' ? { products: [] } : {})
         })
     
-
-
-
     if (userCreate.role === 'customer') {
         userCreate.products = undefined;
         await userCreate.save();  
@@ -54,12 +53,14 @@ const register = async(req,res)=>{
         message:"User registered successfully",
         data:userCreate
     })
+   } catch (error) {
+    console.log(error);
+    
+   }
 
 }
 
-
-
-
+//login user
 const login = async(req,res)=>{
     const {email,password} = req.body
     if(!email) return res.status(404).json({message:"Please enter a email"})
@@ -68,6 +69,7 @@ const login = async(req,res)=>{
     const user = await User.findOne({email: email})
     if(!user) return res.status(400).json({message:"User not found"})
 
+   try {
     const isPassword = await bcrypt.compare(password, user.password)
     if(!isPassword) return res.status(400).json({message:"incorrect password"})
 
@@ -89,13 +91,19 @@ const login = async(req,res)=>{
         data: user
 
     })
+   } catch (error) {
+    console.log(error);
+    
+   }
 }
 
+//logout user
 const logout = async (req,res)=>{
     res.clearCookie("refreshToken",{path:"/"})
     res.status(200).json({message:"User logged out successfully"})
 }
 
+//refresh user token
 const refresh = async (req,res)=>{
     const refreshToken = req.cookies.refreshToken || req.body.refreshToken
     if(!refreshToken) return res.status(403).json({message:"No refresh token found"})
